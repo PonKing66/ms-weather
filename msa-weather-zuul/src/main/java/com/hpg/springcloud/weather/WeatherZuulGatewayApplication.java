723d88b36ef072718594281c13537231c5ee9b68
A <34815351+PonKing66@users.noreply.github.com>
2020-08-28 08:36:47 +0800

    first commit

---
+++ msa-weather-zuul/src/main/java/com/hpg/springcloud/weather/WeatherZuulGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.hpg.springcloud.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * @author Ponking
+ * @ClassName EurekaServerApplication
+ * @date 2020/5/9--18:09
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class WeatherZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherZuulGatewayApplication.class, args);
+    }
+}
